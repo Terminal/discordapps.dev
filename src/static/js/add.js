65fc47a957bfb38ba9e73d514a6d7294c952615a
lepon01 <leondrolio@gmail.com>
2017-09-02 16:00:42 +0100

    Complete english locale

--- src/static/js/add.js
+++ src/static/js/add.js
@@ -1,19 +1,12 @@
 /* eslint no-unused-vars: 0 */
 
-const name = document.getElementById('name');
-const avatar = document.getElementById('avatar');
-const invite = document.getElementById('invite');
-const botid = document.getElementById('id');
-const count = document.getElementById('count');
-const shortDesc = document.getElementById('shortDesc');
 const type = document.getElementById('type');
 const descbox = document.getElementById('description');
-const errorbox = document.getElementById('error');
 
 const description = () => {
 	if (type.value === 'iframe') {
 		descbox.innerHTML = '<input type="text" class="form-control" id="longDesc" name="longDesc" maxlength="200" required pattern="https:\\/\\/.+">';
 	} else if (type.value === 'markdown') {
-		descbox.innerHTML = '<textarea class="form-control" id="longDesc" name="longDesc" maxlength="20000" rows="6" required></textarea><p><a href="https://guides.github.com/features/mastering-markdown/" target="_blank">GitHub Markdown help</a></p>';
+		descbox.innerHTML = '<textarea class="form-control" id="longDesc" name="longDesc" maxlength="20000" rows="6" required></textarea><p><a href="https://guides.github.com/features/mastering-markdown/" target="_blank">GitHub Markdown</a></p>';
 	}
 };
