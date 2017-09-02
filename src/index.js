4adfee4a3d3fdb2f198d97687ba00017d3cd5ef5
lepon01 <leondrolio@gmail.com>
2017-09-02 17:08:43 +0100

    Add comments

--- src/index.js
+++ src/index.js
@@ -1,23 +1,54 @@
+// Parser for POST and cookies
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const config = require('config');
-const cons = require('consolidate');
-const express = require('express');
-const session = require('express-session');
-// const bot = require('./bot');
-const path = require('path');
-const r = require('./db');
-const apiR = require('./api');
-const authR = require('./auth');
-const csrfR = require('./csrf');
-const docsR = require('./docs');
-const userR = require('./user');
-const discR = require('./discord');
-const langM = require('./lang');
-const auth = require('./auth/auth');
-const crypto = require('crypto');
-const i18n = require('i18n');
 
+// Configuration
+const config = require('config');
+
+// Consolidation of pug files to HTML
+const cons = require('consolidate');
+
+// Webserver
+const express = require('express');
+
+// Sessions for express
+const session = require('express-session');
+
+// Eris Discord Bot
+// bot.guild - The Terminal guild
+// bot.channel - The selected channel
+const bot = require('./bot');
+
+// Joining paths
+const path = require('path');
+
+// RethinkDB
+const r = require('./db');
+
+// API Middleware
+const apiM = require('./api');
+
+// Authentication Middleware
+const auth = require('./auth/auth');
+const authM = require('./auth');
+const userM = require('./user');
+const csrfM = require('./csrf');
+
+// Documentation Middleware
+const docsM = require('./docs');
+
+// Bot List Middleware
+const discM = require('./discord');
+
+// Crypto for generating API tokens when a bot is created
+const crypto = require('crypto');
+
+// Internationalisation
+const i18n = require('i18n');
+const langM = require('./lang');
+
+// Configure i18n to NEVER touch the files, and default to
+// English UK
 i18n.configure({
 	directory: path.join(__dirname, '..', 'locales'),
 	cookie: 'lang',
@@ -26,36 +57,39 @@ i18n.configure({
 	updateFiles: false
 });
 
+// Create an express app
 const app = express();
 
-app.set('views', path.join(__dirname, 'dynamic'))
-	.engine('html', cons.pug)
+app.set('views', path.join(__dirname, 'dynamic')) // Allocate views to be used
+	.engine('html', cons.pug) // Use PUG
 	.set('view engine', 'html')
-	.use(cookieParser(config.get('webserver').secret))
+	.use(cookieParser(config.get('webserver').secret)) // Set cookie secret
 	.use(session({
 		secret: config.get('webserver').secret,
 		resave: true,
 		saveUninitialized: true,
 		proxy: true
 	}))
-	.use(i18n.init)
-	.use(auth.initialize())
+	.use(i18n.init) // Use i18n
+	.use(auth.initialize()) // Initiate the authentication mechanisms
 	.use(auth.session())
 	.use(bodyParser.json())
 	.use(bodyParser.urlencoded({
 		extended: true
 	}))
-	.use(userR.userSetup)
-	.get('/', csrfR.make, (req, res, next) => {
+	.use(userM.userSetup) // Append details such as if they are an admin, and if they are in the guild
+	.get('/', csrfM.make, (req, res, next) => {
 		res.locals.approve = true;
 		next();
-	}, discR.list)
-	.get('/all', csrfR.make, discR.list)
-	.get('/queue', csrfR.make, (req, res, next) => {
+	}, discM.list) // Display approved bots
+	.get('/all', csrfM.make, discM.list) // Display all bots
+	.get('/queue', csrfM.make, (req, res, next) => {
+		// Display not-yet approved bots
 		res.locals.approve = false;
 		next();
-	}, discR.list)
-	.post('/queue', userR.auth, csrfR.check, userR.admin, (req, res) => {
+	}, discM.list)
+	.post('/queue', userM.auth, csrfM.check, userM.admin, (req, res) => {
+		// Delete or flag bot as approved
 		const previous = req.header('Referer') || '/';
 		if (typeof req.body.id.length > 70
 			|| (req.body.approve !== 'true' && req.body.approve !== 'false')) {
@@ -73,7 +107,7 @@ app.set('views', path.join(__dirname, 'dynamic'))
 						res.status(500).render('error.pug', { status: 500, message: 'An error occured while updating bot info into Rethonk DB' });
 					} else {
 						res.redirect(previous);
-						// bot.channel.createMessage(`<@${req.user.id}> approved \`${result.changes[0].old_val.name}\` <@${result.changes[0].old_val.id}> by <@${result.changes[0].old_val.owner}>`);
+						bot.channel.createMessage(`<@${req.user.id}> approved \`${result.changes[0].old_val.name}\` <@${result.changes[0].old_val.id}> by <@${result.changes[0].old_val.owner}>`);
 					}
 				});
 		} else if (req.body.approve === 'false') {
@@ -87,20 +121,23 @@ app.set('views', path.join(__dirname, 'dynamic'))
 						res.status(500).render('error.pug', { status: 500, message: 'An error occured while deleting bot info into Rethonk DB' });
 					} else {
 						res.redirect(previous);
-						// bot.channel.createMessage(`<@${req.user.id}> rejected \`${result.changes[0].old_val.name}\` <@${result.changes[0].old_val.id}> by <@${result.changes[0].old_val.owner}>`);
+						bot.channel.createMessage(`<@${req.user.id}> rejected \`${result.changes[0].old_val.name}\` <@${result.changes[0].old_val.id}> by <@${result.changes[0].old_val.owner}>`);
 					}
 				});
 		} else {
 			res.status(500).render('error.pug', { status: 500, message: 'An invalid approval type was encountered that was not caught earlier' });
 		}
 	})
-	.get('/add', userR.auth, csrfR.make, (req, res) => {
+	.get('/add', userM.auth, csrfM.make, (req, res) => {
+		// Display the add screen
 		res.render('add.pug', {
 			csrf: req.csrf,
 			title: 'Add Bot'
 		});
 	})
-	.post('/add', userR.auth, csrfR.check, discR.validate, (req, res) => {
+	.post('/add', userM.auth, csrfM.check, discM.validate, (req, res) => {
+		// Insert specific elements into the database.
+		// Input validated by Discord Middleware
 		r.table('bots')
 			.insert({
 				id: req.body.id,
@@ -123,18 +160,21 @@ app.set('views', path.join(__dirname, 'dynamic'))
 					res.status(409).render('error.pug', { status: 409, message: 'A bot with this ID already exists in the database.' });
 				} else {
 					res.render('error.pug', { status: 200, message: 'Thanks. That went well.' });
-					// bot.channel.createMessage(`<@${req.user.id}> added \`${req.body.name}\` <@${req.body.id}>`);
+					// Send message to Discord Channel
+					bot.channel.createMessage(`<@${req.user.id}> added \`${req.body.name}\` <@${req.body.id}>`);
 				}
 			});
 	})
-	.get('/edit/:id', userR.auth, csrfR.make, discR.owns, (req, res) => {
+	.get('/edit/:id', userM.auth, csrfM.make, discM.owns, (req, res) => {
+		// Display the edit screen with the bot's items
 		res.render('edit.pug', {
 			csrf: req.csrf,
 			bot: res.locals.bot,
 			title: 'Edit Bot'
 		});
 	})
-	.post('/edit/:id', userR.auth, csrfR.check, discR.owns, discR.validate, (req, res) => {
+	.post('/edit/:id', userM.auth, csrfM.check, discM.owns, discM.validate, (req, res) => {
+		// Edit only the bits that need to be edited
 		r.table('bots')
 			.get(req.body.id)
 			.update({
@@ -153,17 +193,19 @@ app.set('views', path.join(__dirname, 'dynamic'))
 					res.render('error.pug', { status: 200, message: 'Your bot was left unchanged.' });
 				} else {
 					res.render('error.pug', { status: 200, message: 'Thanks. That went well.' });
-					// bot.channel.createMessage(`<@${req.user.id}> edited \`${res.locals.bot.name}\` <@${res.locals.bot.id}> by <@${res.locals.bot.owner}>`);
+					bot.channel.createMessage(`<@${req.user.id}> edited \`${res.locals.bot.name}\` <@${res.locals.bot.id}> by <@${res.locals.bot.owner}>`);
 				}
 			});
 	})
-	.get('/delete/:id', userR.auth, csrfR.make, discR.owns, (req, res) => {
+	.get('/delete/:id', userM.auth, csrfM.make, discM.owns, (req, res) => {
+		// View a page before deleting the bot
 		res.render('delete.pug', {
 			csrf: req.csrf,
 			title: 'Delete Bot'
 		});
 	})
-	.post('/delete/:id', userR.auth, csrfR.check, discR.owns, (req, res) => {
+	.post('/delete/:id', userM.auth, csrfM.check, discM.owns, (req, res) => {
+		// Delete the bot
 		r.table('bots')
 			.get(req.params.id)
 			.delete()
@@ -172,17 +214,20 @@ app.set('views', path.join(__dirname, 'dynamic'))
 					res.status(500).render('error.pug', { status: 500, message: 'An error occured while inserting bot info into Rethonk DB' });
 				} else {
 					res.render('error.pug', { status: 200, message: 'Your bot was successfully deleted.' });
-					// bot.channel.createMessage(`<@${req.user.id}> deleted \`${res.locals.bot.name}\` <@${res.locals.bot.id}> by <@${res.locals.bot.owner}>`);
+					bot.channel.createMessage(`<@${req.user.id}> deleted \`${res.locals.bot.name}\` <@${res.locals.bot.id}> by <@${res.locals.bot.owner}>`);
 				}
 			});
 	})
-	.get('/token/:id', userR.auth, csrfR.make, discR.owns, (req, res) => {
+	.get('/token/:id', userM.auth, csrfM.make, discM.owns, (req, res) => {
+		// Display the token for this bot
 		res.render('token.pug', {
 			csrf: req.csrf,
-			bot: res.locals.bot
+			bot: res.locals.bot,
+			title: 'Token'
 		});
 	})
-	.post('/token/:id', userR.auth, csrfR.check, discR.owns, (req, res) => {
+	.post('/token/:id', userM.auth, csrfM.check, discM.owns, (req, res) => {
+		// Replace the token for this bot.
 		r.table('bots')
 			.get(req.params.id)
 			.update({
@@ -194,16 +239,18 @@ app.set('views', path.join(__dirname, 'dynamic'))
 				if (err) {
 					res.status(500).render('error.pug', { status: 500, message: 'An error occured while updating bot info into Rethonk DB' });
 				} else {
+					// Go back to the token screen
 					res.redirect(`/token/${req.params.id}`);
 				}
 			});
 	})
-	.use('/api', apiR)
-	.use('/auth', authR)
-	.use('/docs', docsR)
-	.use('/lang', langM)
-	.use(express.static(path.join(__dirname, 'static')))
+	.use('/api', apiM) // API
+	.use('/auth', authM) // Authentication
+	.use('/docs', docsM) // Documentation
+	.use('/lang', langM) // Language settings
+	.use(express.static(path.join(__dirname, 'static'))) // Pull static files from /src/static
 	.use('*', (req, res) => {
+		// Give off a 404 if the chain ends here
 		res.status(404).render('error.pug', { status: 404, message: 'Not found' });
 	})
 	.listen(config.get('webserver').port);
