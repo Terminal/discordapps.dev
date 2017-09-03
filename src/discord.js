eb2e77996caea094a433c3db5132b0c4206a8912
lepon01 <leondrolio@gmail.com>
2017-09-03 11:32:25 +0100

    Autofill optional fields

--- src/discord.js
+++ src/discord.js
@@ -2,6 +2,8 @@
 const r = require('./db');
 const mustache = require('mustache');
 const marked = require('marked');
+const request = require('request');
+const config = require('config');
 
 marked.setOptions({
 	sanitize: true
@@ -76,12 +78,6 @@ const validate = (req, res, next) => {
 		res.status(400).render('error.pug', { status: 400, message: 'You provided an invalid ID' });
 	} else if (typeof req.body.shortDesc !== 'string') {
 		res.status(400).render('error.pug', { status: 400, message: 'You provided an invalid short description' });
-	} else if (typeof req.body.name !== 'string') {
-		res.status(400).render('error.pug', { status: 400, message: 'You provided an invalid name' });
-	} else if (typeof req.body.avatar !== 'string') {
-		res.status(400).render('error.pug', { status: 400, message: 'You provided an invalid avatar' });
-	} else if (typeof req.body.invite !== 'string') {
-		res.status(400).render('error.pug', { status: 400, message: 'You provided an invalid invite' });
 	} else if (typeof req.body.type !== 'string') {
 		res.status(400).render('error.pug', { status: 400, message: 'You provided an invalid type' });
 	} else if (req.body.type !== 'iframe' && req.body.type !== 'markdown') {
@@ -94,20 +90,12 @@ const validate = (req, res, next) => {
 		res.status(400).render('error.pug', { status: 400, message: 'You provided a bot id that was too long (70)' });
 	} else if (req.body.shortDesc.length > 200) {
 		res.status(400).render('error.pug', { status: 400, message: 'You provided a short description that was too long (200)' });
-	} else if (req.body.name.length > 32) {
-		res.status(400).render('error.pug', { status: 400, message: 'You provided a name that was too long (32)' });
 	} else if (req.body.avatar.length > 2000) {
 		res.status(400).render('error.pug', { status: 400, message: 'You provided an avatar that was too long (2000)' });
-	} else if (req.body.invite.length > 2000) {
-		res.status(400).render('error.pug', { status: 400, message: 'You provided an invite that was too long (2000)' });
 	} else if (parseInt(req.body.count, 10) < 0) {
 		res.status(400).render('error.pug', { status: 400, message: 'Your bot count was too low (0)' });
 	} else if (parseInt(req.body.count, 10) > 1000000) {
 		res.status(400).render('error.pug', { status: 400, message: 'Your bot count was too high (1000000)' });
-	} else if (!/^https:\/\//.test(req.body.avatar)) {
-		res.status(400).render('error.pug', { status: 400, message: 'Your avatar must use HTTPS' });
-	} else if (!/^https?:\/\//.test(req.body.invite)) {
-		res.status(400).render('error.pug', { status: 400, message: 'Your invite must use HTTP or HTTPS' });
 	} else if (req.body.type === 'iframe' && !/^https:\/\//.test(req.body.longDesc)) {
 		res.status(400).render('error.pug', { status: 400, message: 'Your iframe based long description must use HTTPS' });
 	} else if (req.body.type === 'iframe' && req.body.longDesc > 2000) {
@@ -117,7 +105,55 @@ const validate = (req, res, next) => {
 	} else if (/\D/.test(req.body.id)) {
 		res.status(400).render('error.pug', { status: 400, message: 'Your bot ID had values other than digits' });
 	} else {
-		next();
+		if (!req.body.invite) { // If there is no invite, make one up using the ID.
+			req.body.invite = `https://discordapp.com/oauth2/authorize?client_id=${req.body.id}&scope=bot&permissions=0`;
+		}
+
+		if (typeof req.body.invite !== 'string') {
+			res.status(400).render('error.pug', { status: 400, message: 'You provided an invalid invite' });
+		} else if (req.body.invite.length > 2000) {
+			res.status(400).render('error.pug', { status: 400, message: 'You provided an invite that was too long (2000)' });
+		} else if (!/^https?:\/\//.test(req.body.invite)) {
+			res.status(400).render('error.pug', { status: 400, message: 'Your invite must use HTTP or HTTPS' });
+		} else {
+			request({
+				uri: `https://discordapp.com/api/v6/users/${req.body.id}`,
+				method: 'GET',
+				headers: {
+					'User-Agent': config.get('useragent'),
+					Authorization: `Bot ${config.get('discord').token}`
+				},
+				json: true
+			}, (err, response, body) => {
+				console.dir(body);
+
+				if (!req.body.avatar) {
+					req.body.avatar = `https://cdn.discordapp.com/avatars/${body.id}/${body.avatar}`;
+				}
+
+				if (!req.body.name) {
+					req.body.name = body.username;
+				}
+
+				if (response.statusCode === 404) {
+					res.status(404).render('error.pug', { status: 404, message: 'Discord could not find your bot.' });
+				} else if (response.statusCode !== 200) {
+					res.status(500).render('error.pug', { status: 500, message: `Discord returned error ${response.statusCode}` });
+				} else if (!body.bot) {
+					res.status(400).render('error.pug', { status: 400, message: 'Userbots are not allowed' });
+				} else if (typeof req.body.avatar !== 'string') {
+					res.status(400).render('error.pug', { status: 400, message: 'You provided an invalid avatar' });
+				} else if (!/^https:\/\//.test(req.body.avatar)) {
+					res.status(400).render('error.pug', { status: 400, message: 'Your avatar must use HTTPS' });
+				} else if (typeof req.body.name !== 'string') {
+					res.status(400).render('error.pug', { status: 400, message: 'You provided an invalid name' });
+				} else if (req.body.name.length > 32) {
+					res.status(400).render('error.pug', { status: 400, message: 'You provided a name that was too long (32)' });
+				} else {
+					next();
+				}
+			});
+		}
 	}
 };
 
