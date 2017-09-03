c35c44b4f3413d88c5ab36a8814f93beeeac0303
lepon01 <leondrolio@gmail.com>
2017-09-03 11:30:07 +0100

    Add not found if skipped

--- src/index.js
+++ src/index.js
@@ -105,6 +105,8 @@ app.set('views', path.join(__dirname, 'dynamic')) // Allocate views to be used
 				.run(r.conn, (err, result) => {
 					if (err) {
 						res.status(500).render('error.pug', { status: 500, message: 'An error occured while updating bot info into Rethonk DB' });
+					} else if (res.skipped) {
+						res.status(404).render('error.pug', { status: 404, message: 'Bot Not found' });
 					} else {
 						res.redirect(previous);
 						bot.channel.createMessage(`<@${req.user.id}> approved \`${result.changes[0].old_val.name}\` <@${result.changes[0].old_val.id}> by <@${result.changes[0].old_val.owner}>`);
@@ -123,6 +125,8 @@ app.set('views', path.join(__dirname, 'dynamic')) // Allocate views to be used
 				.run(r.conn, (err, result) => {
 					if (err) {
 						res.status(500).render('error.pug', { status: 500, message: 'An error occured while deleting bot info into Rethonk DB' });
+					} else if (res.skipped) {
+						res.status(404).render('error.pug', { status: 404, message: 'Bot Not found' });
 					} else {
 						res.redirect(previous);
 						bot.channel.createMessage(`<@${req.user.id}> rejected \`${result.changes[0].old_val.name}\` <@${result.changes[0].old_val.id}> by <@${result.changes[0].old_val.owner}>`);
