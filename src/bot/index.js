6c2b7b7299a39b9b18b96de7375e268640ab1b57
lepon01 <leondrolio@gmail.com>
2017-09-03 01:33:08 +0100

    Add autorole

--- src/bot/index.js
+++ src/bot/index.js
@@ -2,6 +2,8 @@ const Discord = require('eris');
 const config = require('config');
 
 const client = new Discord.Client(config.get('discord').token);
+let tguild = null;
+let tchannel = null;
 
 client.on('ready', () => {
 	console.log('Discord Bot is online');
@@ -11,9 +13,18 @@ client.on('ready', () => {
 		type: 0
 	});
 
-	module.exports.client = client;
-	module.exports.guild = client.guilds.get(config.get('discord').guild);
-	module.exports.channel = module.exports.guild.channels.get(config.get('discord').channel);
+	tguild = client.guilds.get(config.get('discord').guild);
+	tchannel = tguild.channels.get(config.get('discord').channel);
+});
+
+client.on('guildMemberAdd', (guild, member) => {
+	if (tguild.id === guild.id && member.bot) {
+		member.addRole(config.get('discord').bot);
+	}
 });
 
 client.connect();
+
+module.exports.client = client;
+module.exports.tguild = tguild;
+module.exports.tchannel = tchannel;
