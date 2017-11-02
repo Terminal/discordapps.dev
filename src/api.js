8bee501a5d167faf6798527e0244985036d10747
lepon01 <leondrolio@gmail.com>
2017-11-02 16:27:27 +0000

    Use async

--- src/api.js
+++ src/api.js
@@ -1,87 +1,86 @@
+/**
+
+MIT License
+
+Copyright (c) 2017 Moustacheminer Server Services
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 const express = require('express');
 const r = require('./db');
 
 const router = express.Router();
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
 	const auth = req.get('Authorization');
-	if (!auth) {
-		res.status(401).json({ error: 'You need to provide a token. View /docs/api' });
-	} else if (typeof auth !== 'string') {
-		res.status(401).json({ error: 'Invalid Authorisation header.' });
+
+	const bot = await r.table('bots')
+		.get(req.params.id)
+		.run(r.conn);
+
+	if (!bot) {
+		res.status(404).json({ error: 'This bot doesn\'t exist' });
+	} else if (auth === bot.token) {
+		next();
 	} else {
-		r.table('bots')
-			.get(req.params.id)
-			.run(r.conn, (err, bot) => {
-				if (err) {
-					res.status(500).json({ error: err.message });
-				} else if (!bot) {
-					res.status(404).json({ error: 'This bot doesn\'t exist' });
-				} else if (auth === bot.token) {
-					next();
-				} else {
-					res.status(401).json({ error: 'Incorrect Authorisation header.' });
-				}
-			});
+		res.status(401).json({ error: 'Incorrect Authorisation header.' });
 	}
 };
 
 router.get('/', (req, res) => {
 	res.redirect('/docs/api');
 })
-	.get('/bots', (req, res) => {
-		r.table('bots')
+	.get('/bots', async (req, res) => {
+		const cursor = await r.table('bots')
 			.without('token')
-			.run(r.conn, (err1, cursor) => {
-				if (err1) {
-					res.status(500).json({ error: err1.message });
-				} else {
-					cursor.toArray((err2, result) => {
-						if (err2) {
-							res.status(500).json({ error: err2.message });
-						} else {
-							res.status(200).send(result);
-						}
-					});
-				}
-			});
+			.run(r.conn);
+		const result = await cursor.toArray();
+		res.status(200).send(result);
 	})
 	.get('/bots/:id', (req, res) => {
-		r.table('bots')
+		const result = r.table('bots')
 			.get(req.params.id)
-			.default({})
 			.without('token')
-			.run(r.conn, (err, result) => {
-				if (err) {
-					res.status(500).json({ error: err.message });
-				} else if (!result) {
-					res.status(404).json({});
-				} else {
-					res.status(200).json(result);
-				}
-			});
+			.run(r.conn);
+
+		if (!result) {
+			res.status(404).json({});
+		} else {
+			res.status(200).json(result);
+		}
 	})
-	.post('/bots/:id', authMiddleware, (req, res) => {
-		const count = req.body.count || req.body.server_count;
+	.post('/bots/:id', authMiddleware, async (req, res) => {
+		const count = parseInt(req.body.count || req.body.server_count, 10);
 		if (typeof count !== 'string' && typeof count !== 'number') {
 			res.status(400).json({ error: 'You provided an invalid guild count' });
-		} else if (parseInt(count, 10) < 0) {
+		} else if (count < 0) {
 			res.status(400).json({ error: 'Your bot count was too low (0)' });
-		} else if (parseInt(count, 10) > 1000000) {
+		} else if (count > 1000000) {
 			res.status(400).json({ error: 'Your bot count was too high (1000000)' });
 		} else {
-			r.table('bots')
+			await r.table('bots')
 				.get(req.params.id)
-				.update({
-					count: parseInt(count, 10),
-				})
-				.run(r.conn, (err) => {
-					if (err) {
-						res.status(500).json({ error: err.message });
-					} else {
-						res.status(200).json({ message: 'OK' });
-					}
-				});
+				.update({ count })
+				.run(r.conn);
+			res.status(200).json({ message: 'OK' });
 		}
 	})
 	.use('/test/:id', authMiddleware, (req, res) => {
