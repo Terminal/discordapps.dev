cd65ea5ca6686afff698380806c5b8aa10dfea57
lepon01 <leondrolio@gmail.com>
2017-11-06 23:29:36 +0000

    Quickly bodge in a discord bot (since we can)

---
+++ src/listbot/cogs.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const commands = {};
+const categories = {};
+
+// Register valid commands from "cogs"
+fs.readdir('./src/listbot/cogs/', (err, items) => {
+	items.forEach((item) => {
+		const file = item.replace(/\.js/g, '');
+		const cog = require(`./cogs/${file}`); // eslint-disable-line global-require, import/no-dynamic-require
+		categories[file] = cog;
+		cog.forEach((com) => {
+			com.aliases.forEach((alias) => {
+				if (commands[alias]) {
+					throw new Error(`Alias ${alias} from ${file} was already assigned to another command!`);
+				} else {
+					commands[alias] = com;
+				}
+			});
+		});
+	});
+});
+
+module.exports = { commands, categories };
--- src/listbot/index.js
+++ src/listbot/index.js
@@ -1,5 +1,7 @@
 const Discord = require('eris');
 const config = require('config');
+const { commands } = require('./cogs');
+const handler = require('./handler');
 
 const client = new Discord.Client(config.get('discord').token);
 
@@ -7,13 +9,20 @@ client.on('ready', () => {
 	console.log('Discord Bot is online');
 
 	client.editStatus('online', {
-		name: 'list.mss.ovh',
+		name: config.get('discord').game,
 		type: 0
 	});
 
 	module.exports.guild = client.guilds.get(config.get('discord').guild);
 	module.exports.channel = module.exports.guild.channels.get(config.get('discord').channel);
 	module.exports.ready = true;
+
+	client.on('messageCreate', (message) => {
+		handler(message);
+		if (message.mss.command && message.mss.admin >= commands[message.mss.command].admin) {
+			commands[message.mss.command].command(message);
+		}
+	});
 });
 
 client.connect();
