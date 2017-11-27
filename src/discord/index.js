343694e083b74cde1e5b3f09cce07e6043f58de1
7coil <leondrolio@gmail.com>
2017-11-27 16:23:13 +0000

    Never use a return. They are evil.

--- src/discord/index.js
+++ src/discord/index.js
@@ -27,11 +27,7 @@ client.on('ready', () => {
 	});
 
 	client.on('userUpdate', (user, old) => {
-		if (!user || !old) {
-			return;
-		}
-
-		if (user.avatar !== old.avatar) {
+		if (user && old && (user.avatar !== old.avatar) {
 			r.table('bots')
 				.get(user.id)
 				.update({
