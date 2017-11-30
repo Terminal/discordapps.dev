4160416ff0aabede6c3e385cacd781369836a7e8
Devoxin <lukel9914@gmail.com>
2017-11-30 01:33:23 +0000

    Automatically fill in DB & Tables

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-require('./src/index');
+const config = require('config');
+const defaultConfig = config.get('rethinkdb');
+
+const rConfig = {servers: defaultConfig.servers}
+const r = require('rethinkdbdash')(rConfig);
+
+verifyDb();
+
+async function verifyDb() {
+	const shouldConfigure = !(await r.dbList().contains('terminal').run());
+	if (shouldConfigure) {
+		await r.dbCreate(defaultConfig.db);
+		['bots', 'csrf', 'users', 'session', 'i18n'].map(async (table) => {
+			await r.db(defaultConfig.db).tableCreate(table).run();
+		});
+	}
+
+	r.getPoolMaster().drain();
+	delete r;
+}
+
+
+//require('./src/index');
