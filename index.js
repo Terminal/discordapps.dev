3fe8dacd65eb79c54643121ce46c18fd38244a85
7coil <leondrolio@gmail.com>
2017-12-20 16:42:55 +0000

    Add manifest, change image on error

--- index.js
+++ index.js
@@ -17,4 +17,28 @@ const verifyDb = async () => {
 };
 
 verifyDb();
+
+console.log('\x1b[0m');
+
+console.log('  Welcome to ls.terminal.ink!');
+console.log('\x1b[0m');
+console.log('  \x1b[0m  \x1b[1m\x1b[32m\x1b[46m\u2588\u2588\u2588\u2588\u2588\u2588\u2593\u2593\u2593\u2593\u2593\u2593\u2592\u2592\u2592\u2592\u2592\u2592\u2591\u2591\u2591\u2591\u2591\u2591      \x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m\u2588\u2588\x1b[0m                              \x1b[1m\x1b[36m\x1b[44m\u2588\u2588\x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m\u2588\u2588\x1b[0m   ▞                          \x1b[1m\x1b[36m\x1b[44m\u2588\u2588\x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m\u2588\u2588\x1b[0m  ▟▙▖                         \x1b[1m\x1b[36m\x1b[44m\u2588\u2588\x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m\u2593\u2593\x1b[0m   ▛                          \x1b[1m\x1b[36m\x1b[44m\u2593\u2593\x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m\u2593\u2593\x1b[0m  ▝   \x1b[5m▀▀▀\x1b[0m                     \x1b[1m\x1b[36m\x1b[44m\u2593\u2593\x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m\u2593\u2593\x1b[0m                              \x1b[1m\x1b[36m\x1b[44m\u2593\u2593\x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m\u2592\u2592\x1b[0m                              \x1b[1m\x1b[36m\x1b[44m\u2592\u2592\x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m\u2592\u2592\x1b[0m                              \x1b[1m\x1b[36m\x1b[44m\u2592\u2592\x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m\u2592\u2592\x1b[0m                              \x1b[1m\x1b[36m\x1b[44m\u2592\u2592\x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m\u2591\u2591\x1b[0m                              \x1b[1m\x1b[36m\x1b[44m\u2591\u2591\x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m\u2591\u2591\x1b[0m                              \x1b[1m\x1b[36m\x1b[44m\u2591\u2591\x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m\u2591\u2591\x1b[0m                              \x1b[1m\x1b[36m\x1b[44m\u2591\u2591\x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m  \x1b[0m                              \x1b[1m\x1b[36m\x1b[44m  \x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m  \x1b[0m                              \x1b[1m\x1b[36m\x1b[44m  \x1b[0m');
+console.log('  \x1b[0m\x1b[1m\x1b[32m\x1b[46m  \x1b[0m                              \x1b[1m\x1b[36m\x1b[44m  \x1b[0m');
+console.log('  \x1b[0m  \x1b[1m\x1b[36m\x1b[44m\u2588\u2588\u2588\u2588\u2588\u2588\u2593\u2593\u2593\u2593\u2593\u2593\u2592\u2592\u2592\u2592\u2592\u2592\u2591\u2591\u2591\u2591\u2591\u2591      \x1b[0m');
+console.log('\x1b[0m');
+
 require('./src/index');
