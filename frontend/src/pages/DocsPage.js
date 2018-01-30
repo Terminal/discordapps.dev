41899b6c95b8d7f6a0e653b8fc8f8510cab26371
7coil <leondrolio@gmail.com>
2018-01-30 00:25:16 +0000

    I don't know what I'm doing

---
+++ frontend/src/pages/DocsPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class DocsPage extends Component {
+	render() {
+		return (
+			<div>
+				<h4>Introduction</h4>
+				<p>
+					Somebody once told me the world was gonna roll me
+					He ain't the smartest tool in the shed
+					She was looking kinda dumb
+					With her finger and her thumb
+					With a shape of an L on her forehead
+				</p>
+			</div>
+		);
+	}
+}
