307857b726efde3e246a80d75209d245a95206d4
lepon01 <leondrolio@gmail.com>
2018-02-13 00:57:34 +0000

    Knock knock. Who's there? GNU GPL v3.

--- src/discord/cogs/list.js
+++ src/discord/cogs/list.js
@@ -1,4 +1,23 @@
-//	const client = require('./..');
+/**
+	ls.terminal.ink Discord Bot List Server
+	Copyright (C) 2018 Moustacheminer Server Services
+	Copyright (C) 2018 Terminal.ink
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 const r = require('./../../db');
 const config = require('config');
 
