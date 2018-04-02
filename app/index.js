afc0122cc3f0b16d61ece2bebff4d3f3f934ef21
Leondro Lio <leondrolio@gmail.com>
2018-04-02 01:09:04 +0100

    People don't need to see the token

--- app/index.js
+++ app/index.js
@@ -6,13 +6,19 @@ const r = require('./modules/rethinkdb');
 const app = express();
 
 app.use(bodyParser.json())
+  .set('json spaces', 2)
   .get('/', (req, res) => {
     res.json({
       message: 'Welcome to the ls.terminal.ink API server',
     });
   })
   .get('/bots', (req, res) => {
-    r.table('bots').run().then((bots) => {
-      res.json(bots);
-    });
+    r.table('bots')
+      .without('token')
+      .run()
+      .then((bots) => {
+        res.json(bots);
+      });
   });
+
+app.listen(8080);
