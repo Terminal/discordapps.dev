7be251dd8568ddd4570ff71b81e3bb9c59e01d51
lepon01 <katie@discordmail.com>
2018-11-18 03:09:27 +0000

    Add magical links

--- website/schemas/bots.js
+++ website/schemas/bots.js
@@ -28,24 +28,20 @@ const schema = joi.object({
   oauth: joi.string().regex(/^[0-9]+$/, 'numbers').allow(null).error(new Error('errors.bots.oauth')),
   invite: joi.string().uri({ scheme: ['https'] }).required().error(new Error('errors.bots.invite')),
   support: joi.string().uri({ scheme: ['https'] }).allow(null).error(new Error('errors.bots.support')),
-  authors: joi.array().items(joi.string().regex(/^[0-9]+$/, 'numbers')).min(1).max(10)
-    .error(new Error('errors.bots.authors')),
+  authors: joi.array().items(joi.string().regex(/^[0-9]+$/, 'numbers')).min(1).max(10).required().error(new Error('errors.bots.authors')),
   nsfw: joi.bool().error(new Error('errors.bots.nsfw')),
   github: joi.object({
     owner: joi.string().regex(githubUsernameRegex, 'github username').allow(null).error(new Error('errors.bots.githubowner')),
     repo: joi.string().allow(null).error(new Error('errors.bots.githubrepo')),
   }),
   trigger: joi.object({
-    prefix: joi.array().items(joi.string().min(1).max(10)).min(1).max(10)
-      .error(new Error('errors.bots.prefix')),
+    prefix: joi.array().items(joi.string().min(1).max(10)).required().min(1).max(10).required().error(new Error('errors.bots.prefix')),
     customisable: joi.bool().error(new Error('errors.bots.customisable')),
     mentionable: joi.bool().error(new Error('errors.bots.mentionable')),
   }),
   images: joi.object({
-    avatar: joi.string().uri({ scheme: ['https'] }).max(2000).allow(null)
-      .error(new Error('errors.bots.avatar')),
-    cover: joi.string().uri({ scheme: ['https'] }).max(2000).allow(null)
-      .error(new Error('errors.bots.cover')),
+    avatar: joi.string().uri({ scheme: ['https'] }).max(2000).allow(null).error(new Error('errors.bots.avatar')),
+    cover: joi.string().uri({ scheme: ['https'] }).max(2000).allow(null).error(new Error('errors.bots.cover')),
     preview: joi.array().items(joi.string().uri({ scheme: ['https'] }).max(2000)).max(20).error(new Error('errors.bots.preview'))
   }),
   flags: joi.object({
@@ -53,12 +49,9 @@ const schema = joi.object({
     adverts: joi.bool().error(new Error('errors.bots.adverts'))
   }),
   contents: joi.object().pattern(joi.string().valid(Object.keys(config.languages)), joi.object({
-    name: joi.string().min(4).max(32).required()
-      .error(new Error('errors.bots.name')),
-    description: joi.string().min(10).max(100).required()
-      .error(new Error('errors.bots.description')),
-    page: joi.string().min(20).max(10000).required()
-      .error(new Error('errors.bots.page')),
+    name: joi.string().min(4).max(32).required().error(new Error('errors.bots.name')),
+    description: joi.string().min(10).max(100).required().error(new Error('errors.bots.description')),
+    page: joi.string().min(20).max(10000).required().error(new Error('errors.bots.page')),
   })).required().error((errors) => {
     if (errors.some(e => e.context.key === 'contents')) {
       return new Error('errors.bots.languages');
