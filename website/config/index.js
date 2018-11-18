b5b36f098e810b21d42a2743346b6a1db5b44018
lepon01 <katie@discordmail.com>
2018-11-18 00:11:02 +0000

    fix eslint errors

--- website/config/index.js
+++ website/config/index.js
@@ -4,5 +4,5 @@ const production = require('./production.json');
 if (process.env.NODE_ENV === 'development') {
   module.exports = Object.assign(development, production);
 } else {
-	module.exports = Object.assign(production, development);
+  module.exports = Object.assign(production, development);
 }
