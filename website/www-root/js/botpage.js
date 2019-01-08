3f767ee74c67aa5401e8efc36f9b0d091301cb0a
lepon01 <katie@discordmail.com>
2019-01-08 02:20:02 +0000

    Use long description for both short and long description on bot pages

--- website/www-root/js/botpage.js
+++ website/www-root/js/botpage.js
@@ -2,19 +2,54 @@
   const more = document.getElementById('ls-more');
   const less = document.getElementById('ls-less');
   const custom = document.getElementById('ls-custom-content');
-  const description = document.getElementById('ls-short-description');
+
+  let allowExtension = true;
+
+  // Calculate the height required to display the children.
+  // Get each child's height, top and bottom margin
+  // Then sum them together
+  const getExtendedHeight = () => [...custom.children]
+    .map((elem) => {
+      const height = elem.clientHeight;
+      const topMargin = parseInt(document.defaultView.getComputedStyle(elem, '').getPropertyValue('margin-top'), 10);
+      const bottomMargin = parseInt(document.defaultView.getComputedStyle(elem, '').getPropertyValue('margin-bottom'), 10);
+
+      return topMargin + height + bottomMargin;
+    })
+    .reduce((prev, curr) => prev + curr, 0);
+
+  // Set the height, if the contents is extended
+  const setExtendedHeight = () => {
+    if (allowExtension) {
+      custom.style.height = `${getExtendedHeight()}px`;
+    }
+  };
 
   more.addEventListener('click', () => {
-    description.classList.add('hidden');
-    custom.classList.remove('hidden');
+    allowExtension = true;
+    setExtendedHeight();
     more.classList.add('hidden');
     less.classList.remove('hidden');
   });
 
+  // When shrinking, unextend
   less.addEventListener('click', () => {
-    custom.classList.add('hidden');
-    description.classList.remove('hidden');
+    allowExtension = false;
+    custom.style.height = '150px';
     less.classList.add('hidden');
     more.classList.remove('hidden');
+  });
+
+  window.addEventListener('resize', () => {
+    setExtendedHeight();
+  });
+
+  window.addEventListener('load', () => {
+    if (getExtendedHeight() < 150) {
+      allowExtension = true;
+      less.classList.add('hidden');
+      more.classList.add('hidden');
+      setExtendedHeight();
+    }
   });
 }
