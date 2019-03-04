39c76ec20021c8d72592f82a8dd04e9984b08efe
lepon01 <katie@discordmail.com>
2019-03-04 18:14:42 +0000

    can i merge these commits

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,7 @@ import reducer from './reducers';
 let composeEnhancers = compose;
 
 // If within a web browser...
-if (typeof window === 'undefined') {
+if (typeof window !== 'undefined') {
 
   // If the Development tools is installed, hook into that
   if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
