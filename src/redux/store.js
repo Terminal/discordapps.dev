21ad1ff079b6b4af9a60b3ca77e5cdb831004e0a
lepon01 <katie@discordmail.com>
2019-03-04 18:11:51 +0000

    please

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,16 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 
-const composeEnhancers = typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose || compose;
+let composeEnhancers = compose;
+
+// If within a web browser...
+if (typeof window !== 'undefined') {
+
+  // If the Development tools is installed, hook into that
+  if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+  }
+}
 
 export default function configureStore() {
   return createStore(
