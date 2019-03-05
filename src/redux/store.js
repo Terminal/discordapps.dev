8e45624ee2e3fd9a4361082c18813821f7503d75
lepon01 <katie@discordmail.com>
2019-03-05 01:26:03 +0000

    using the power of magic, put the devtools AFTER thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,18 @@
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
+
+const stuffToCompose = [
+  applyMiddleware(thunk)
+];
+
+if (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__) {
+  stuffToCompose.push(window.__REDUX_DEVTOOLS_EXTENSION__());
+}
 
 export default function configureStore() {
   return createStore(
     reducer,
-    composeWithDevTools(
-      applyMiddleware(thunk)
-    )
+    compose(...stuffToCompose)
   );
 }
