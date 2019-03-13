da5f6ef59f430077408409c4f8fece590014bf38
lepon01 <katie@discordmail.com>
2019-03-13 02:38:13 +0000

    add a plugin

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -4,7 +4,13 @@ require('@babel/polyfill');
 
 require('@babel/register')({
   ignore: [ /(node_modules)/ ],
-  presets: ['@babel/preset-env', '@babel/preset-react']
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react'
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties'
+  ]
 });
 
 require('./index');
