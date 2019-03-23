6941f7dd0d3985c90c6bc6138f87a68bf9073de7
lepon01 <katie@discordmail.com>
2019-03-23 15:20:56 +0000

    remove unused parts of ls10 for ls13-server

--- website/express.js
+++ website/express.js
@@ -3,10 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const sass = require('node-sass-middleware');
 const cookieParser = require('cookie-parser');
 const RDBStore = require('session-rethinkdb')(session);
-const handlebars = require('express-handlebars');
 
 const r = require('./rethinkdb');
 const i18n = require('../global/i18n');
@@ -19,10 +17,7 @@ const sitemapRouter = require('./routers/sitemap');
 const languageMiddleware = require('./middleware/language');
 // const getBetterLanguageMiddleware = require('./middleware/getBetterLanguage');
 
-const dateformat = require('./data/dateformat.json');
 const selectableStates = require('./data/states.json');
-
-const alternativeSites = require('./data/sites.json');
 const allowedCors = require('./data/cors.json');
 
 require('./static/banner');
@@ -35,51 +30,8 @@ app.locals.defaultLanguage = config.default.language;
 app.locals.defaultImage = config.default.image;
 app.locals.selectableStates = selectableStates;
 
-app.set('views', path.join(path.dirname(__filename), 'views'))
-  .set('view engine', 'handlebars')
+app
   .set('json spaces', 4)
-  .engine('handlebars', handlebars({
-    defaultLayout: 'main',
-    layoutsDir: path.join(path.dirname(__filename), 'views', 'layouts'),
-    partialsDir: path.join(path.dirname(__filename), 'views', 'partials'),
-    helpers: {
-      i18n: (translator, name, ...args) => {
-        const replacement = {};
-        for (let i = 0; i < args.length; i += 2) {
-          replacement[args[i]] = args[i + 1];
-        }
-        if (typeof translator === 'function') {
-          return translator(name, replacement);
-        }
-        return args[0] || 'Translation Error!';
-      },
-      i18ns: (translator, ...args) => {
-        if (typeof translator === 'function') {
-          return translator(...args);
-        }
-        return args[0] || 'Translation Error!';
-      },
-      date: (date, getLocale) => (new Date(date)).toLocaleDateString(getLocale(), dateformat),
-      stringify: (...args) => JSON.stringify(...args),
-      concat: (...args) => args.slice(0, -1).join(''),
-      isArray: value => Array.isArray(value),
-      encodeURIComponent: value => encodeURIComponent(value),
-      or: (var1, var2) => var1 || var2,
-      and: (var1, var2) => var1 && var2,
-      isEqual: (var1, var2) => var1 === var2,
-      not: var1 => !var1,
-      add: (var1, var2) => var1 + var2,
-      languages: () => i18n.getLocales(),
-      webserverLocation: () => config.webserver.location,
-      do: (n, block) => {
-        let accumulator = '';
-        for (let i = 0; i < n; i += 1) {
-          accumulator += block.fn(i);
-        }
-        return accumulator;
-      }
-    },
-  }))
   .use(cors({
     origin(origin, callback) {
       if (allowedCors.indexOf(origin) !== -1) {
