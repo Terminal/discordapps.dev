b201a0deefd856d84909a7612e0dfbf5acd81af7
lepon01 <katie@discordmail.com>
2019-03-23 00:27:27 +0000

    use class to make component

--- src/pages/Locale.js
+++ src/pages/Locale.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import Container from '../components/Container';
 import ContentBox from '../components/ContentBox';
@@ -6,18 +6,24 @@ import FlagLinks from '../components/FlagLinks';
 import Layout from '../components/Layout';
 import Modesta from '../data/Modesta';
 
-export default () => (
-  <Layout match={this.props.match}>
-    <Container>
-      <ContentBox className={Modesta.center}>
-        <h2><FormattedMessage id="pages.locale.choose" /></h2>
-        <FlagLinks />
-        <p>
-          <a href="https://github.com/Terminal/ls.terminal.ink/tree/master/locales">
-            <FormattedMessage id="pages.locale.pleasehelp"></FormattedMessage>
-          </a>
-        </p>
-      </ContentBox>
-    </Container>
-  </Layout>
-);
+class Locale extends Component {
+  render() {
+    return (
+      <Layout match={this.props.match}>
+        <Container>
+          <ContentBox className={Modesta.center}>
+            <h2><FormattedMessage id="pages.locale.choose" /></h2>
+            <FlagLinks />
+            <p>
+              <a href="https://github.com/Terminal/ls.terminal.ink/tree/master/locales">
+                <FormattedMessage id="pages.locale.pleasehelp"></FormattedMessage>
+              </a>
+            </p>
+          </ContentBox>
+        </Container>
+      </Layout>
+    )
+  }
+}
+
+export default Locale;
