759d500198a7170df30e9788e4ac619fc4b51cdf
lepon01 <katie@discordmail.com>
2019-03-29 18:40:05 +0000

    show reviews only if available, add all preview images and avatar

--- src/helpers/reviewToJsonLd.js
+++ src/helpers/reviewToJsonLd.js
@@ -6,33 +6,40 @@ const reviewToJsonLd = (contents, item) => {
     '@type': 'Product',
     description: contents.description,
     name: contents.name,
-    image: `${Locations.server}${item.cachedImages.avatar}`,
+    image: [
+      `${Locations.server}${item.cachedImages.avatar}`,
+      ...item.cachedImages.preview.map(img => `${Locations.server}${img}`)
+    ],
     sku: item.id
   };
+  
+  // If there are reviews, add the reviews stuff
+  if (item.reviews.length) {
+    const average = item.reviews.reduce((prev, curr) => prev + curr.rating, 0) / item.reviews.length;
+    
+    template.aggregateRating = {
+      '@type': 'AggregateRating',
+      ratingValue: average,
+      reviewCount: item.reviews.length
+    };
 
-  const average = item.reviews.reduce((prev, curr) => prev + curr.rating, 0) / item.reviews.length;
-
-  template.aggregateRating = {
-    '@type': 'AggregateRating',
-    ratingValue: average,
-    reviewCount: item.reviews.length
-  };
-  template.review = [];
-  for (let i = 0; i < item.reviews.length; i += 1) {
-    const review = item.reviews[i];
-    const date = new Date(review.date);
-    template.review.push({
-      '@type': 'Review',
-      author: review.username,
-      datePublished: `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`,
-      description: review.text,
-      reviewRating: {
-        '@type': 'Rating',
-        bestRating: '5',
-        ratingValue: review.rating.toString(),
-        worstRating: '1'
-      }
-    });
+    template.review = [];
+    for (let i = 0; i < item.reviews.length; i += 1) {
+      const review = item.reviews[i];
+      const date = new Date(review.date);
+      template.review.push({
+        '@type': 'Review',
+        author: review.username,
+        datePublished: `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`,
+        description: review.text,
+        reviewRating: {
+          '@type': 'Rating',
+          bestRating: '5',
+          ratingValue: review.rating.toString(),
+          worstRating: '1'
+        }
+      });
+    }
   }
 
   return JSON.stringify(template);
